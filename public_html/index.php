a7e3942c21b6ced2d6783a6bb068d37d89bbfc43
Ben <hostellerie.org@gmail.com>
2026-09-22 18:01:52 +0700

    Simplify and harden PayPal storefront flow

--- public_html/index.php
+++ public_html/index.php
@@ -75,10 +75,9 @@ paypal_filterVars($vars, $_REQUEST);
 
 $display = '';
 
-if ($_REQUEST['n'] == '') {
-    $pageTitle = $_PAY_CONF['seo_shop_title'];
-} else {
-    $pageTitle = $_REQUEST['n'] . ' | ' . $_PAY_CONF['seo_shop_title'];
+$pageTitle = $_PAY_CONF['seo_shop_title'];
+if ($_REQUEST['n'] !== '') {
+    $pageTitle = $_REQUEST['n'] . ' | ' . $pageTitle;
 }
 
 if (SEC_hasRights('paypal.user,paypal.admin', 'OR')) {
@@ -89,23 +88,33 @@ if (SEC_hasRights('paypal.user,paypal.admin', 'OR')) {
 
 switch ($_REQUEST['mode']) {
     case 'endTransaction':
-	    // START SESSION
-        if (session_status() !== PHP_SESSION_ACTIVE) {
-            session_start();
+        PAYPAL_getCart()->empty_cart();
+
+        $txnId = isset($_POST['txn_id']) ? $_POST['txn_id'] : '';
+        $firstName = isset($_POST['first_name']) ? $_POST['first_name'] : '';
+        $lastName = isset($_POST['last_name']) ? $_POST['last_name'] : '';
+        $payerEmail = isset($_POST['payer_email']) ? $_POST['payer_email'] : '';
+        $currency = isset($_POST['mc_currency']) ? $_POST['mc_currency'] : '';
+        $gross = isset($_POST['mc_gross']) ? $_POST['mc_gross'] : '';
+        $itemCount = isset($_POST['num_cart_items']) ? (int) $_POST['num_cart_items'] : 0;
+
+        $msg = $LANG_PAYPAL_1['thanks_details'];
+        $msg .= '<p>' . $LANG_PAYPAL_1['transaction'] . ' ' . $txnId . '</p>';
+        $msg .= '<p>' . $LANG_PAYPAL_1['name_label'] . ' ' . trim($firstName . ' ' . $lastName)
+            . ' | ' . $LANG_PAYPAL_1['email'] . ' ' . $payerEmail . '</p>';
+
+        if ($itemCount > 0) {
+            $msg .= '<ul>';
+            for ($i = 1; $i <= $itemCount; $i++) {
+                $quantity = isset($_POST["quantity{$i}"]) ? $_POST["quantity{$i}"] : '';
+                $itemName = isset($_POST["item_name{$i}"]) ? $_POST["item_name{$i}"] : '';
+                $itemGross = isset($_POST["mc_gross_{$i}"]) ? $_POST["mc_gross_{$i}"] : '';
+                $msg .= '<li>' . $quantity . 'x ' . $itemName . '... ' . $itemGross . ' ' . $currency . '</li>';
+            }
+            $msg .= '</ul>';
         }
-		// INITIALIZE JCART AFTER SESSION START
-		$cart =& $_SESSION['jcart']; 
-		if(!($cart instanceof jcart)) $cart = new jcart();
-		// EMPTY THE CART
-		$cart->empty_cart();
-		
-		$msg = $LANG_PAYPAL_1['thanks_details'];
-		$msg .= '<p>' . $LANG_PAYPAL_1['transaction'] . ' ' . $_POST['txn_id'] . '</p>';
-		$msg .= '<p>' . $LANG_PAYPAL_1['name_label'] . ' ' . $_POST['first_name'] . ' ' . $_POST['last_name'] . ' | ' . $LANG_PAYPAL_1['email'] . ' ' . $_POST['payer_email'] . '</p><ul>';
-		for ($i = 1; $i <= $_POST['num_cart_items']; $i++) {
-            $msg .= '<li>' . $_POST["quantity$i"] . 'x '. $_POST["item_name$i"] . '... '  . $_POST["mc_gross_$i"] . ' ' . $_POST['mc_currency'];
-        }
-		$msg .=  '</ul><p>' . $LANG_PAYPAL_1['total']  . ' ' . $_POST['mc_gross'] . ' ' . $_POST['mc_currency'] . '</p>';
+
+        $msg .= '<p>' . $LANG_PAYPAL_1['total'] . ' ' . $gross . ' ' . $currency . '</p>';
         $display .= COM_showMessageText($msg, $LANG_PAYPAL_1['thanks']);
         break;
 	
@@ -116,10 +125,15 @@ switch ($_REQUEST['mode']) {
         break;
 		
 	default :
-	    if ($_PAY_CONF['paypal_main_header'] != '' && $_REQUEST['category'] == '') $display .= '<div>' . PLG_replaceTags($_PAY_CONF['paypal_main_header']) . '</div>';
-		$display .= PAYPAL_displayProducts('',0,$_REQUEST['category']);
-        
-		if ($_PAY_CONF['paypal_main_footer'] != '') $display .= '<div>' . PLG_replaceTags($_PAY_CONF['paypal_main_footer']) . '</div>';
+        if ($_PAY_CONF['paypal_main_header'] !== '' && $_REQUEST['category'] === '') {
+            $display .= '<div>' . PLG_replaceTags($_PAY_CONF['paypal_main_header']) . '</div>';
+        }
+
+        $display .= PAYPAL_displayProducts('', 0, $_REQUEST['category']);
+
+        if ($_PAY_CONF['paypal_main_footer'] !== '') {
+            $display .= '<div>' . PLG_replaceTags($_PAY_CONF['paypal_main_footer']) . '</div>';
+        }
 		
 }
 
