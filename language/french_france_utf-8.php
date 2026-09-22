d3b2e0b73eaa1fef45e1f737ea5709bddd81462c
Ben <hostellerie.org@gmail.com>
2026-09-22 10:41:56 +0700

    Fix PayPal subscription language key

--- language/french_france_utf-8.php
+++ language/french_france_utf-8.php
@@ -416,7 +416,7 @@ $LANG_PAYPAL_CART = array(
 
 $LANG_PAYPAL_TYPE = array(
     'product'                 => 'Product',
-	'subscrition'             => 'Subscription',
+	'subscription'             => 'Subscription',
 	'donation'                => 'Donation',
 	'rent'                    => 'Rent',
 	'recurrent'               => 'Payement récurrent'
